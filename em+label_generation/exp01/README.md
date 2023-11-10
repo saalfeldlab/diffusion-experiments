b11300546d0fb11166d48d8615b2326ae17c8c0d
Larissa Heinrich <heinrichl@janelia.hhmi.org>
2023-11-10 15:35:39 -0500

    feat: :alembic: add exp1

---
+++ em+label_generation/exp01/README.md
@@ -0,0 +1,9 @@
+# exp01
+
+-----
+## Experiment description
+A diffusion network to generate EM and corresponding labels. List of labels is the simple, mutually exlusive list.
+
+## License
+
+`exp01` is distributed under the terms of the [MIT](https://spdx.org/licenses/MIT.html) license.
---
+++ em+label_generation/exp01/src/exp01/train.py
@@ -0,0 +1,72 @@
+import os
+from urllib.parse import urlparse, urlunparse
+
+import mlflow
+import yaml
+from denoising_diffusion_pytorch import GaussianDiffusion, Trainer, Unet
+
+from exp01.config import (
+    ExperimentConfig,
+    TrackingConfig,
+)
+from exp01.utility import flatten_dict, get_repo_and_commit_cwd
+
+def track(config: TrackingConfig):
+    parsed_uri = urlparse(config.tracking_uri)
+    if parsed_uri.scheme == "file":
+        print(os.path.exists(parsed_uri.path))  # this is just accessing it for autofs
+    mlflow.set_tracking_uri(config.tracking_uri)
+    mlflow.set_experiment(config.experiment_name)
+    if config.continue_run_id is None:
+        with mlflow.start_run(run_name=config.run_name) as run:
+            return run.info.run_id
+    else:
+        #    with mlflow.start_run(run_id = config.continue_run_id) as run:
+        return config.continue_run_id
+
+
+def run():
+    # Load configuration from YAML file
+    with open("experiment_config.yaml") as config_file:
+        yaml_data = yaml.safe_load(config_file)
+    config = ExperimentConfig(**yaml_data)
+
+    run_id = track(config.tracking)
+    with mlflow.start_run(run_id=run_id):
+        repo, commit_hash = get_repo_and_commit_cwd()
+        mlflow.log_param("repo", repo)
+        mlflow.log_param("commit", commit_hash)
+
+        mlflow.log_params(flatten_dict(config.model_dump()))
+        mlflow.pytorch.autolog()
+        architecture = config.architecture.get_constructor()(
+            **config.architecture.model_dump()
+        )
+
+        diffusion = config.diffusion.get_constructor()(
+            architecture, image_size=config.image_size, **config.diffusion.model_dump()
+        )
+        data_args = config.data
+        data_args["image_size"] = config.image_size
+        dataset = config.data.get_constructor()(**data_args)
+        crop_list = []
+        for ds in dataset:
+            crop_list.extend([c.crop_name for c in ds.crops])
+            
+        mlflow.log_param("crop_list", crop_list)
+        parsed_artifact_uri = urlparse(mlflow.get_artifact_uri())
+        if parsed_artifact_uri.scheme != "file":
+            raise NotImplementedError(
+                f"Using a {parsed_artifact_uri.scheme} connection to save artifacts is not implemented"
+            )
+        trainer = Trainer(
+            diffusion,
+            dataset,
+            results_folder=parsed_artifact_uri.path,
+            **config.training.model_dump(),
+        )
+        trainer.train()
+
+
+if __name__ == "__main__":
+    run()
